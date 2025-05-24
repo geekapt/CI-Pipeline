c7d4efb0614576fb4b7ac9ebc3c0945ea45bcdd4
geekapt <35554539+geekapt@users.noreply.github.com>
2025-05-24 20:08:07 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from GitHub CI/CD Demo!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
+
